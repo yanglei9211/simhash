978a64adca0a9e5da9fcdacb2678302481b8761b
yanglei <lei.yang@17zuoye.com>
2017-06-02 14:38:02 +0800

    update simhash

--- cluster.go
+++ cluster.go
@@ -16,9 +16,8 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
 	beego.Run()
 	*/
-	s1 := "发觉肯定是离开家理发店addfadf沙发的设计科罗拉多放假开始54564654654"
+	s1 := "54564654654"
 	ts := simhash.Simhash{}
 	ts.Init(s1)
-	res := ts.Slide()
-	fmt.Println(res)
+	fmt.Println(ts.Value())
 }
--- simhash/simhash.go
+++ simhash/simhash.go
@@ -5,18 +5,21 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
-	"go/token"
 )
 
 const f = 64
 const win_size = 4
-const reg = "[\u4e00-\u9fcca-zA-Z0-9_]+"
+//const reg = "[\u4e00-\u9fcca-zA-Z0-9_]+"
+const reg = `[\w\p{Han}]+`
 
-func hashfunc(x string) string{
+func hashfunc(x string) uint64{
 	h := md5.New()
 	h.Write([]byte(x))
 	r := h.Sum(nil)
-	return fmt.Sprintf("%x", r)
+	var res uint64
+	rs := fmt.Sprintf("%x", r[8:])
+	fmt.Sscanf(rs, "%x", &res)
+	return res
 }
 
 type Simhash struct {
@@ -27,15 +30,21 @@ type Simhash struct {
 }
 
 func (s *Simhash) Init(data string) {
+	fmt.Println(reg)
 	r := regexp.MustCompile(reg)
+	fmt.Println(r)
 	res := r.FindAllString(data, -1)
 	for _, rs := range res {
 		s.data += rs
 	}
 	s.win_size = win_size
 	s.f = f
+	s.buildByText()
 }
 
+func (s *Simhash) Value() uint64 {
+	return s.value
+}
 
 func (s *Simhash) Tokenize() {
 	rs := strings.ToLower(s.data)
@@ -44,18 +53,39 @@ func (s *Simhash) Tokenize() {
 		p := rs[st:st+s.win_size]
 		res = append(res, p)
 	}
-	s.dataToken = res
+	s.features = res
 }
 
 func (s *Simhash) buildByText() {
-
+	s.Tokenize()
+	s.buildByFeatures()
 }
 
 func (s *Simhash) buildByFeatures() {
-	hashs := make([]string, 0, len(s.features))
-	for w := range(s.features){
+	hashs := make([]uint64, 0, len(s.features))
+	for _, w := range(s.features){
 		hashs = append(hashs, hashfunc(w))
 	}
 	v := make([]int, s.f)
-
+	masks := make([]uint64, 0, s.f)
+	for i := 0; i < s.f; i++ {
+		masks = append(masks, 1<<uint(i))
+	}
+	for _, h := range hashs {
+		for i := 0; i < s.f; i++ {
+			if (h & masks[i]) > 0 {
+				v[i]++
+			} else {
+				v[i]--
+			}
+		}
+	}
+	fmt.Println(v)
+	var ans uint64
+	for i := 0; i < s.f; i++ {
+		if v[i] >= 0 {
+			ans |= masks[i]
+		}
+	}
+	s.value = ans
 }
